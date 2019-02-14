177f82440f0c60700b78e454e9072923f6b424e0
Artur-yurii KORCHYNSKYI <akorchyn@e2r10p1.unit.ua>
2019-02-14 17:29:41 +0200

    Start point

--- Corewar/includes/vm.h
+++ Corewar/includes/vm.h
@@ -6,7 +6,7 @@
 /*   By: akorchyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:38:06 by kpshenyc          #+#    #+#             */
-/*   Updated: 2019/02/14 16:50:25 by akorchyn         ###   ########.fr       */
+/*   Updated: 2019/02/14 17:09:45 by akorchyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,32 @@
 # include "op.h"
 # include "../libft/includes/libft.h"
 
+# define ERROR(x, str) if (x) {write(2, str, ft_strlen(str)); exit(x);}
+
+# define DEBUG 1
+
+# pragma pack(1)
+
 typedef struct			s_carriage
 {
 	struct s_carriage	*next;
+	void				*reg;
 	int32_t				counter;
 	int32_t				step_size;
 	int32_t 			last_live;
 	int32_t 			pause;
+	int16_t				size;
 	int8_t				player;
 	uint8_t				carry : 1;
-	void				*reg;
-
 }						t_carriage;
 
 typedef struct			s_corewar
 {
 	char				*map;
 	t_carriage			*carriages;
-	uint8_t				is_dump : 1;
 	int32_t				dump_drop;
 	int32_t				iteration;
+	uint8_t				is_dump : 1;
 }						t_corewar;
 
 #endif
