f95a043e7a7cbad97b36c18d87cf922011c26c1d
dmlitvin <dmitri.litvinenko.2000@gmail.com>
2019-03-14 10:55:52 +0200

    testednb1

--- newasm/name_comment.c
+++ newasm/name_comment.c
@@ -31,46 +31,49 @@ static void	collect_name(char const *q1, char const *q2,
 		throw_error("double name declaration", g_line);
 	else if (content_size > PROG_NAME_LENGTH)
 		throw_error("too big bot name", g_line);
-	else if (!content_size)
-		throw_error("empty bot name", g_line);
 	else
 		ft_memcpy(g_header.prog_name, q1 + 1, q2 - q1 - 1);
+	while (q1 != q2)
+		if (*q1++ == '\n')
+			g_line++;
 }
 
 static void	collect_comment(char const *q1, char const *q2,
 		const size_t content_size)
 {
-	if (!content_size)
-		throw_error("empty bot comment", g_line);
-	else if (content_size > COMMENT_LENGTH)
+	if (content_size > COMMENT_LENGTH)
 		throw_error("too big bot comment", g_line);
 	else if (g_header.comment[0])
 		throw_error("double bot comment declaration", g_line);
 	else
 		ft_memcpy(g_header.comment, q1 + 1, q2 - q1 - 1);
+	while (q1 != q2)
+		if (*q1++ == '\n')
+			g_line++;
 }
 
-static void	collect_directive(char *start, char *end)
+static char	*collect_directive(char *start)
 {
 	char	*quote1;
 	char	*quote2;
-	char	*str;
 
-	str = get_clear_string(start, end);
-	quote1 = ft_strchr(str, '\"');
-	quote2 = (quote1) ? ft_strrchr(str, '\"') : NULL;
-	if (!quote1 || quote1 == quote2)
+	while (ft_is_whitespace(start) && *start)
+		start++;
+	quote1 = ft_strchr(start, '\"');
+	quote2 = (quote1) ? ft_strchr(quote1 + 1, '\"') : NULL;
+	if (!quote2)
 		throw_error("bad name or comment", g_line);
-	if (!ft_strncmp(str, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING))
-			&& ft_is_whitespace(str + ft_strlen(NAME_CMD_STRING)))
+	if (!ft_strncmp(start, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING))
+			&& ft_is_whitespace(start + ft_strlen(NAME_CMD_STRING)))
 		collect_name(quote1, quote2, quote2 - quote1 - 1);
-	else if (!ft_strncmp(str, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING))
-			&& ft_is_whitespace(str + ft_strlen(COMMENT_CMD_STRING)))
+	else if (!ft_strncmp(start, COMMENT_CMD_STRING,
+			ft_strlen(COMMENT_CMD_STRING))
+			&& ft_is_whitespace(start + ft_strlen(COMMENT_CMD_STRING)))
 		collect_comment(quote1, quote2, quote2 - quote1 - 1);
 	else
 		throw_error("First two instructions must "
 				"include bot name and comment!", 0);
-	free(str);
+	return (quote2 + 1);
 }
 
 char		*read_name_comment(char *start, uint8_t counter)
@@ -84,13 +87,10 @@ char		*read_name_comment(char *start, uint8_t counter)
 		end = ft_strchr(start, '\n');
 		if (line_not_clear(start, end))
 		{
-			collect_directive(start, end);
+			end = collect_directive(start);
 			counter--;
 		}
 		start = (end) ? end + 1 : end;
 	}
-	if (!g_header.prog_name[0] || !g_header.comment[0])
-		throw_error("First two instructions must "
-					"include bot name and comment!", 0);
 	return (end);
 }
